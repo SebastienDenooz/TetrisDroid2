bae79398e6622f91be348044785e18b997903636
Sébastien Denooz <sebastien.denooz@lattirail.net>
2013-10-22 10:44:21 +0200

    Update the layout, add class for the core game and make some update

---
+++ src/com/game/TetrisDroid2/Game.java
@@ -0,0 +1,44 @@
+package com.game.TetrisDroid2;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+public class Game {
+
+    private static LinearLayout gameBoard;
+    private static Context context;
+    private static int [] gameBoardDimension = {10,20};
+
+
+    public static void main( LinearLayout _gameBoard, Context _context) {
+        gameBoard = _gameBoard;
+        context = _context;
+
+        for (int i = 0; i <= gameBoardDimension[0] -1; i++){
+
+            LinearLayout verticalLine = new LinearLayout(context);
+
+            verticalLine.setId(i);
+            verticalLine.setOrientation(LinearLayout.VERTICAL);
+            verticalLine.setWeightSum(gameBoardDimension[1]);
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.FILL_PARENT, 1);
+            verticalLine.setLayoutParams(params);
+
+            gameBoard.addView(verticalLine);
+
+            for (int j = 0; j <= gameBoardDimension[1]-1; j++){
+
+                LinearLayout gridCase = new LinearLayout(context);
+                gridCase.setOrientation(LinearLayout.VERTICAL);
+
+                LinearLayout.LayoutParams caseParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT, 1);
+                gridCase.setLayoutParams(caseParams);
+                verticalLine.addView(gridCase);
+                gridCase.setWeightSum(1);
+            }
+
+        }
+    }
+
+}
--- src/com/game/TetrisDroid2/Tetris.java
+++ src/com/game/TetrisDroid2/Tetris.java
@@ -2,6 +2,7 @@ package com.game.TetrisDroid2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class Tetris extends Activity {
     /**
@@ -11,5 +12,10 @@ public class Tetris extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        LinearLayout gameBoard = (LinearLayout) this.findViewById(R.id.gameBoard);
+        Game theGame = new Game();
+        theGame.main(gameBoard, this);
+
     }
 }
